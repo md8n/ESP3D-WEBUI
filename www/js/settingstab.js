4b9921e0a83c333a060ad220d66ca284dc871771
md8n <lee@md8n.com>
2024-11-27 08:09:01 +0900

    settingstab removal of `on`*

--- www/js/settingstab.js
+++ www/js/settingstab.js
@@ -7,6 +7,9 @@ import { id } from "./util";
 
 /** Set up the event handlers for the settings tab */
 const settingstab = () => {
+    id("nvs_setting_filter").addEventListener("click", (event) => build_HTML_setting_list('nvs'));
+    id("tree_setting_filter").addEventListener("click", (event) => build_HTML_setting_list('tree'));
+
     id("settings_status_btn").addEventListener("click", (event) => statusdlg());
     id("settings_SPIFFS_btn").addEventListener("click", (event) => SPIFFSdlg('/'));
     id("settings_update_fw_btn").addEventListener("click", (event) => updatedlg());
@@ -15,4 +18,3 @@ const settingstab = () => {
 }
 
 export { settingstab };
-
