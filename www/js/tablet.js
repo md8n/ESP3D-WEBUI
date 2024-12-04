133d0341694deb8f8920bab4ca6898b7804cfe19
BarbourSmith <BarbourSmith@users.noreply.github.com>
2024-12-04 13:10:09 -0800

    Bump version number

--- www/js/tablet.js
+++ www/js/tablet.js
@@ -7,7 +7,7 @@ var gCodeDisplayable = false
 var snd = null
 var sndok = true
 
-var versionNumber = 0.86
+var versionNumber = 0.87
 
 //Print the version number to the console
 let msgWindow = document.getElementById('messages')
