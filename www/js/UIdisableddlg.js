0e20c880f1ab903096c95c856eacd0287a35c227
md8n <lee@md8n.com>
2024-12-08 11:02:34 +0900

    Update the UIDisabled dialog

--- www/js/UIdisableddlg.js
+++ www/js/UIdisableddlg.js
@@ -1,9 +1,17 @@
+// import translate_text_item, id, setHTML, setactiveModal, showModal, saveSerialMessages
+
 //UIdisabled dialog
-function UIdisableddlg(lostcon) {
-    var modal = setactiveModal('UIdisableddlg.html');
-    if (modal == null) return;
-    if (lostcon) {
-        id('disconnection_msg').innerHTML = translate_text_item("Connection lost for more than 20s");
-    }
-    showModal();
-}
+const UIdisableddlg = (lostcon) => {
+	const modal = setactiveModal("UIdisableddlg.html");
+	if (modal == null) {
+		return;
+	}
+
+	id("UIdisabled_reconnect").addEventListener("click", (event) => window.location.reload());
+	id("UIdisabled_save_serial_msg").addEventListener("click", (event) => saveSerialMessages());
+
+	if (lostcon) {
+		setHTML("disconnection_msg", translate_text_item("Connection lost for more than 20s"));
+	}
+	showModal();
+};
