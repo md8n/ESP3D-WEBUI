1d882c2d20806146377e7313dd154ec1b727aa95
md8n <lee@md8n.com>
2024-12-08 11:13:36 +0900

    cleanup updatedlg

--- www/js/updatedlg.js
+++ www/js/updatedlg.js
@@ -17,79 +17,63 @@ import {
 	translate_text_item,
 } from "./common.js";
 
-var update_ongoing = false;
-var current_update_filename = "";
-//update dialog
+let update_ongoing = false;
+let current_update_filename = "";
+
+/** update dialog */
 const updatedlg = () => {
 	const modal = setactiveModal("updatedlg.html");
 	if (modal == null) {
 		return;
 	}
 
-	id("updateDlgCancel").addEventListener("click", (event) =>
-		closeUpdateDialog("cancel"),
-	);
-	id("updateDlgClose").addEventListener("click", (event) =>
-		closeUpdateDialog("cancel"),
-	);
+	id("updateDlgCancel").addEventListener("click", (event) => closeUpdateDialog("cancel"));
+	id("updateDlgClose").addEventListener("click", (event) => closeUpdateDialog("cancel"));
 
 	id("fw-select").addEventListener("change", (event) => checkupdatefile());
-	id("fw_select_files").addEventListener("click", (event) =>
-		document.getElementById("fw-select").click(),
-	);
-	id("fw_file_name").addEventListener("mouseup", (event) =>
-		document.getElementById("fw_select_files").click(),
-	);
-	id("uploadfw-button").addEventListener("click", (event) =>
-		UploadUpdatefile(),
-	);
+	id("fw_select_files").addEventListener("click", (event) => document.getElementById("fw-select").click());
+	id("fw_file_name").addEventListener("mouseup", (event) => document.getElementById("fw_select_files").click());
+	id("uploadfw-button").addEventListener("click", (event) => UploadUpdatefile());
 
 	setHTML("fw_file_name", translate_text_item("No file chosen"));
 	displayNone("prgfw");
 	displayNone("uploadfw-button");
 	setHTML("updatemsg", "");
 	setValue("fw-select", "");
-	setHTML(
-		"fw_update_dlg_title",
-		translate_text_item("ESP3D Update").replace("ESP3D", "FluidNC"),
-	);
+	setHTML("fw_update_dlg_title", translate_text_item("ESP3D Update").replace("ESP3D", "FluidNC"));
 	showModal();
 };
 
 function closeUpdateDialog(msg) {
 	if (update_ongoing) {
-		alertdlg(
-			translate_text_item("Busy..."),
-			translate_text_item("Update is ongoing, please wait and retry."),
-		);
+		alertdlg(translate_text_item("Busy..."), translate_text_item("Update is ongoing, please wait and retry."));
 		return;
 	}
 	closeModal(msg);
 }
 
 function checkupdatefile() {
-	var files = id("fw-select").files;
 	displayNone("updatemsg");
-	if (files.length == 0) {
-		displayNone("uploadfw-button");
-	} else {
-		displayBlock("uploadfw-button");
-	}
-	if (files.length > 0) {
-		if (files.length == 1) {
+	const files = id("fw-select").files;
+	switch (files.length) {
+		case 0:
+			displayNone("uploadfw-button");
+			setHTML("fw_file_name", translate_text_item("No file chosen"));
+			break;
+		case 1:
+			displayBlock("uploadfw-button");
 			setHTML("fw_file_name", files[0].name);
-		} else {
-			var tmp = translate_text_item("$n files");
-			setHTML("fw_file_name", tmp.replace("$n", files.length));
-		}
-	} else {
-		setHTML("fw_file_name", translate_text_item("No file chosen"));
+			break;
+		default:
+			displayBlock("uploadfw-button");
+			setHTML("fw_file_name", translate_text_item("$n files").replace("$n", files.length));
+			break;
 	}
 }
 
 function UpdateProgressDisplay(oEvent) {
 	if (oEvent.lengthComputable) {
-		var percentComplete = (oEvent.loaded / oEvent.total) * 100;
+		const percentComplete = (oEvent.loaded / oEvent.total) * 100;
 		setValue("prgfw", percentComplete);
 		setHTML(
 			"updatemsg",
@@ -114,18 +98,16 @@ function StartUploadUpdatefile(response) {
 	if (common.http_communication_locked) {
 		alertdlg(
 			translate_text_item("Busy..."),
-			translate_text_item(
-				"Communications are currently locked, please wait and retry.",
-			),
+			translate_text_item("Communications are currently locked, please wait and retry."),
 		);
 		return;
 	}
-	var files = id("fw-select").files;
-	var formData = new FormData();
-	var url = "/updatefw";
-	for (var i = 0; i < files.length; i++) {
-		var file = files[i];
-		var arg = "/" + file.name + "S";
+	const files = id("fw-select").files;
+	const formData = new FormData();
+	const url = "/updatefw";
+	for (let i = 0; i < files.length; i++) {
+		const file = files[i];
+		const arg = `/${file.name}S`;
 		//append file size first to check updload is complete
 		formData.append(arg, file.size);
 		formData.append("myfile[]", file, `/${file.name}`);
@@ -137,10 +119,7 @@ function StartUploadUpdatefile(response) {
 	displayBlock("prgfw");
 	if (files.length === 1) current_update_filename = files[0].name;
 	else current_update_filename = "";
-	setHTML(
-		"updatemsg",
-		`${translate_text_item("Uploading")} ${current_update_filename}`,
-	);
+	setHTML("updatemsg", `${translate_text_item("Uploading")} ${current_update_filename}`);
 	SendFileHttp(
 		url,
 		formData,
@@ -153,13 +132,13 @@ function StartUploadUpdatefile(response) {
 function updatesuccess(response) {
 	setHTML("updatemsg", translate_text_item("Restarting, please wait...."));
 	setHTML("fw_file_name", "");
-	var i = 0;
-	var interval;
-	var x = id("prgfw");
+	let i = 0;
+	let interval;
+	const x = id("prgfw");
 	x.max = 10;
-	interval = setInterval(function () {
+	interval = setInterval(() => {
 		i = i + 1;
-		var x = id("prgfw");
+		const x = id("prgfw");
 		x.value = i;
 		setHTML(
 			"updatemsg",
@@ -183,21 +162,12 @@ function updatefailed(error_code, response) {
 	id("fw-select").value = "";
 	const common = new Common();
 	if (common.esp_error_code !== 0) {
-		alertdlg(
-			translate_text_item("Error"),
-			stdErrMsg(`(${common.esp_error_code})`, common.esp_error_message),
-		);
-		setHTML(
-			"updatemsg",
-			translate_text_item("Upload failed : ") + common.esp_error_message,
-		);
+		alertdlg(translate_text_item("Error"), stdErrMsg(`(${common.esp_error_code})`, common.esp_error_message));
+		setHTML("updatemsg", translate_text_item("Upload failed : ") + common.esp_error_message);
 		common.esp_error_code = 0;
 	} else {
 		alertdlg(translate_text_item("Error"), stdErrMsg(error_code, response));
-		setHTML(
-			"updatemsg",
-			stdErrMsg(error_code, response, translate_text_item("Upload failed")),
-		);
+		setHTML("updatemsg", stdErrMsg(error_code, response, translate_text_item("Upload failed")));
 	}
 	conErr(error_code, response);
 	update_ongoing = false;
