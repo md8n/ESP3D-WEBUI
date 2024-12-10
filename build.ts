179f718771ce3848f42a3538f6c1955f213d5598
md8n <lee@md8n.com>
2024-12-11 07:55:44 +0900

    build work for svg

--- build.ts
+++ build.ts
@@ -4,39 +4,63 @@ const cleanDist = () => {
 	console.log("No file delete function in bun yet. So no `cleanDist`");
 };
 
-const loadAndReplaceHTML = async (fileContents: string) => {
-	if (!fileContents.toLowerCase().includes("loadhtml")) {
+const loadAndReplaceHTML = async (filePath: string, fileContents: string) => {
+	const fcLower = fileContents.toLowerCase();
+	const hasLoadHTML = fcLower.includes("loadhtml");
+	const hasSVG = fcLower.includes(".svg");
+	console.log(`Processing '${filePath}'`);
+	if (!hasLoadHTML && !hasSVG) {
 		// Leave the file as-is - and move on
+		console.log(`No 'loadhtml' or '.svg' in '${filePath}'`);
 		return fileContents;
 	}
 
-	// Remove the script that does the html loading - we won't need it after bundling
-	const regexScript = /\<script.*loadhtml.*>\<\/script>/gim;
-	const fcNoLoad = fileContents.replace(regexScript, "");
+	let fcProcessed = fileContents;
 
-	// Now find all of the places where the above script was used
-	const regexHTML =
-		/\<div\s+id\s*=\s*['"](?<htmlpath>.*\.html)['"]\s*class.*loadhtml.*><\/div>/gm;
-	const loadHTMLResults = [...fcNoLoad.matchAll(regexHTML)];
-	if (!loadHTMLResults.length) {
-		// Leave the file as-is-ish - and move on
-		return fcNoLoad;
-	}
+	if (hasLoadHTML) {
+		console.log(`Processing '${filePath}' for included HTML files`);
+		// Remove the script that does the html loading - we won't need it after bundling
+		const regexScript = /\<script.*loadhtml.*>\<\/script>/gim;
+		const fcNoLoad = fileContents.replace(regexScript, "");
 
-	// Finally replace the original `div` with the actual file
-	let fcReplLoad = fcNoLoad;
-	for (let ix = 0; ix < loadHTMLResults.length; ix++) {
-		const lhr = loadHTMLResults[ix];
-		const hFile = Bun.file(lhr[1].replace("./sub/", "./www/sub/"));
-		const hText = await hFile.text();
-		fcReplLoad = fcReplLoad.replace(lhr[0], hText);
-
-		if (hText.includes("loadhtml")) {
-			fcReplLoad = await loadAndReplaceHTML(fcReplLoad);
+		// Now find all of the places where the above script was used
+		const regexHTML =
+			/\<div\s+id\s*=\s*['"](?<htmlpath>.*\.html)['"]\s*class.*loadhtml.*><\/div>/gm;
+		const loadHTMLResults = [...fcNoLoad.matchAll(regexHTML)];
+		if (!loadHTMLResults.length) {
+			// Leave the file as-is-ish - and move on
+			return fcNoLoad;
 		}
+
+		// Finally replace the original `div` with the actual file
+		let fcReplLoad = fcNoLoad;
+		for (let ix = 0; ix < loadHTMLResults.length; ix++) {
+			const lhr = loadHTMLResults[ix];
+			const childFilePath = lhr[1].replace("./sub/", "./www/sub/");
+			const hFile = Bun.file(childFilePath);
+			const hText = await hFile.text();
+			if (hText.includes(".svg")) {
+				const regexSVG = /\<img\s+src\s*=\s*['"](?<svgpath>.*\.svg)['"].*><\/img>/gmi;
+				const findSVGResults = [...hText.matchAll(regexSVG)];
+				if (findSVGResults.length) {
+					console.log(`found that svg in ${childFilePath}`);
+					for (let jx = 0; jx < findSVGResults.length; jx++) {
+						const svr = findSVGResults[jx];
+						console.log(`it is ${svr[1]}`);
+					}
+				}
+			}
+			fcReplLoad = fcReplLoad.replace(lhr[0], hText);
+
+			if (hText.includes("loadhtml")) {
+				fcReplLoad = await loadAndReplaceHTML(childFilePath, fcReplLoad);
+			}
+		}
+
+		fcProcessed = fcReplLoad;
 	}
 
-	return fcReplLoad;
+	return fcProcessed;
 };
 
 const build = async () => {
@@ -64,7 +88,7 @@ const build = async () => {
 						if (file.extension === ".html") {
 							const fc = await file.content;
 
-							processor.writeFile(file.path, await loadAndReplaceHTML(fc));
+							processor.writeFile(file.path, await loadAndReplaceHTML(file.path, fc));
 						}
 					}
 				},
