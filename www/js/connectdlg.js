1d23aacf06aa98aaecf09f981d769c2293ed84d9
md8n <lee@md8n.com>
2024-12-12 08:47:18 +0900

    hunting down problem with connectdlg

--- www/js/connectdlg.js
+++ www/js/connectdlg.js
@@ -9,49 +9,7 @@ const connectdlg = (getFw = true) => {
 
 	id("connectbtn").addEventListener("click", (event) => retryconnect());
 
-    //authentication
-    sublist = tlist[5].split(":");
-    if (sublist.length != 2) {
-        return false;
-    }
-    if ((sublist[0].trim() == "authentication") && (sublist[1].trim() == "yes")) ESP3D_authentication = true;
-    else ESP3D_authentication = false;
-    //async communications
-    if (tlist.length > 6) {
-        sublist = tlist[6].split(":");
-        if ((sublist[0].trim() == "webcommunication") && (sublist[1].trim() == "Async")) async_webcommunication = true;
-        else {
-            async_webcommunication = false;
-            websocket_port = sublist[2].trim();
-            if (sublist.length>3) {
-                websocket_ip = sublist[3].trim();
-            } else {
-                console.log("No IP for websocket, use default");
-                 websocket_ip =  document.location.hostname;
-            }
-        }
-    }
-    if (tlist.length > 7) {
-        sublist = tlist[7].split(":");
-        if (sublist[0].trim() === "hostname") esp_hostname = sublist[1].trim();
-    }
-    
-    if (tlist.length > 8) {
-        sublist = tlist[8].split(":");
-        if (sublist[0].trim() === "axis") {
-            grblaxis = Number.parseInt(sublist[1].trim());
-        }
-    }
-    
-    if (async_webcommunication) {
-        if (window.EventSource) {
-            event_source = new EventSource('/events');
-            event_source.addEventListener('InitID', Init_events, false);
-            event_source.addEventListener('ActiveID', ActiveID_events, false);
-            event_source.addEventListener('DHT', DHT_events, false);
-        }
-    }
-    startSocket();
+	showModal();
 
 	if (getFw) {
 		retryconnect();
