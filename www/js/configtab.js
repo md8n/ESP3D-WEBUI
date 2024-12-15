15109ec40145fbecb350c87ff1ace4ef2efa8de2
md8n <lee@md8n.com>
2024-12-15 17:53:42 +0900

    configtab cleanup

--- www/js/configtab.js
+++ www/js/configtab.js
@@ -1,21 +1,10 @@
-import {
-	Apply_config_override,
-	Delete_config_override,
-	refreshconfig,
-	id,
-} from "./common.js";
+import { Apply_config_override, Delete_config_override, refreshconfig, id } from "./common.js";
 
 /** Set up the event handlers for the config tab */
 const configtab = () => {
-	id("config_refresh_btn").addEventListener("click", (event) =>
-		refreshconfig(),
-	);
-	id("config_apply_override").addEventListener("click", (event) =>
-		Apply_config_override(),
-	);
-	id("config_delete_override").addEventListener("click", (event) =>
-		Delete_config_override(),
-	);
+	id("config_refresh_btn").addEventListener("click", (event) => refreshconfig());
+	id("config_apply_override").addEventListener("click", (event) => Apply_config_override());
+	id("config_delete_override").addEventListener("click", (event) => Delete_config_override());
 };
 
 export { configtab };
