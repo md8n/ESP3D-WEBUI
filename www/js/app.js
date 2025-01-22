4aeb4579952a9f210d5bf5461371db10dd6f52f6
md8n <lee@md8n.com>
2025-01-23 07:32:01 +0900

    Clean up of app.js because lint said so

--- www/js/app.js
+++ www/js/app.js
@@ -1,33 +1,33 @@
-var ESP3D_authentication = false
-var async_webcommunication = false
-var websocket_port = 0
-var websocket_ip = ''
-var esp_hostname = 'ESP3D WebUI'
-var EP_HOSTNAME
-var EP_STA_SSID
-var EP_STA_PASSWORD
-var EP_STA_IP_MODE
-var EP_STA_IP_VALUE
-var EP_STA_GW_VALUE
-var EP_STA_MK_VALUE
-var EP_WIFI_MODE
-var EP_AP_SSID
-var EP_AP_PASSWORD
-var EP_AP_IP_VALUE
-var EP_BAUD_RATE = 112
-var EP_AUTH_TYPE = 119
-var EP_TARGET_FW = 461
-var EP_IS_DIRECT_SD = 850
-var EP_PRIMARY_SD = 851
-var EP_SECONDARY_SD = 852
-var EP_DIRECT_SD_CHECK = 853
-var SETTINGS_AP_MODE = 1
-var SETTINGS_STA_MODE = 2
-var SETTINGS_FALLBACK_MODE = 3
-var last_ping = 0
-var enable_ping = true
-var esp_error_message = ''
-var esp_error_code = 0
+var ESP3D_authentication = false;
+var async_webcommunication = false;
+var websocket_port = 0;
+var websocket_ip = '';
+var esp_hostname = 'ESP3D WebUI';
+var EP_HOSTNAME;
+var EP_STA_SSID;
+var EP_STA_PASSWORD;
+var EP_STA_IP_MODE;
+var EP_STA_IP_VALUE;
+var EP_STA_GW_VALUE;
+var EP_STA_MK_VALUE;
+var EP_WIFI_MODE;
+var EP_AP_SSID;
+var EP_AP_PASSWORD;
+var EP_AP_IP_VALUE;
+var EP_BAUD_RATE = 112;
+var EP_AUTH_TYPE = 119;
+var EP_TARGET_FW = 461;
+var EP_IS_DIRECT_SD = 850;
+var EP_PRIMARY_SD = 851;
+var EP_SECONDARY_SD = 852;
+var EP_DIRECT_SD_CHECK = 853;
+var SETTINGS_AP_MODE = 1;
+var SETTINGS_STA_MODE = 2;
+var SETTINGS_FALLBACK_MODE = 3;
+var last_ping = 0;
+var enable_ping = true;
+var esp_error_message = '';
+var esp_error_code = 0;
 
 //Check for IE
 //Edge
@@ -123,4 +123,4 @@ window.onload = () => {
 		// Ensure that we always break out of this
 		failSafe--;
 	}, 500);
-}
+};
